efb0e290463fa047574bfa2b0b2bf9b4ff910a3b
_Fushigidane_ <84942946+Fushigidane-Snom@users.noreply.github.com>
2023-11-02 15:31:46 -0400

    Fix collision

--- pynecraft/camera.py
+++ pynecraft/camera.py
@@ -50,13 +50,13 @@ class Camera:
         self.view_matrix = self.get_view_matrix()
 
     def check_collision(self, position):
-        return False # Disabled for now
         x, y, z = position
-        for p in ([x+COLLISION_ZONE, y + COLLISION_ZONE, z+COLLISION_ZONE], [x-COLLISION_ZONE, y + COLLISION_ZONE, z-COLLISION_ZONE], [x+COLLISION_ZONE, y-1, z+COLLISION_ZONE], [x-COLLISION_ZONE, y-1, z-COLLISION_ZONE]):
-            chunkPos = ((p[0]) // CHUNK_SIZE, (p[1]) // CHUNK_SIZE, (p[2]) // CHUNK_SIZE)
-            if chunkPos in self.app.world.chunks:
-                if self.app.world.chunks[chunkPos].mesh.blocks[utils.flatten_coord(int(p[0]) % CHUNK_SIZE, int(p[1]) % CHUNK_SIZE, int(p[2]) % CHUNK_SIZE)]:
-                        return True
+        for c in [COLLISION_ZONE, -COLLISION_ZONE]:
+            for p in ([x, y, z], [x + c, y, z], [x, y + c, z], [x, y, z+c], [x, y-1, z], [x+c, y-1, z], [x, y-1+c, z], [x, y-1, z+c]):
+                chunkPos = ((p[0]) // CHUNK_SIZE, (p[1]) // CHUNK_SIZE, (p[2]) // CHUNK_SIZE)
+                if chunkPos in self.app.world.chunks:
+                    if self.app.world.chunks[chunkPos].mesh.blocks[utils.flatten_coord(int(p[0]) % CHUNK_SIZE, int(p[1]) % CHUNK_SIZE, int(p[2]) % CHUNK_SIZE)]:
+                            return True
         return False
 
     def move(self):
