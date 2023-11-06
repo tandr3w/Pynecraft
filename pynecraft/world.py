79d6659dca843ab373d16ed6b70e888364107ce8
_Fushigidane_ <84942946+Fushigidane-Snom@users.noreply.github.com>
2023-11-06 13:02:39 -0500

    Add block generation to async

--- pynecraft/world.py
+++ pynecraft/world.py
@@ -4,14 +4,31 @@ from numba.typed import Dict
 import multiprocessing
 import time
 from OpenGL.GL import *
+import utils
 
-
-def func(x, y):
+def chunk_provider(chunkX, chunkZ, CHUNK_SIZE, CHUNK_HEIGHT, flatten_coord):
+    import glm
     from random import randint
-    g = 1
-    for i in range(1000000):
-        g += randint(1, 10)
-    return x, y, g
+    import numpy as np
+
+    blocks = np.zeros(CHUNK_SIZE ** 2 * CHUNK_HEIGHT, dtype='uint8')
+    for x in range(CHUNK_SIZE):
+        for z in range(CHUNK_SIZE):
+            # 0.02 - Scale, higher = smaller hills in the xy direction (less fat)
+            # 8 - Amplitude, higher = taller hills, lower valleys
+            # 32 - Sea level
+            local_height = int(glm.simplex(glm.vec2(chunkX + x, chunkZ + z) * 0.02) * 8 + 64)
+            if local_height < 0:
+                continue
+            for y in range(min(local_height, CHUNK_HEIGHT)):
+                if local_height - y <= 1:
+                    blocks[flatten_coord(x, y, z)] = 2
+                elif local_height - y <= 3:
+                    blocks[flatten_coord(x, y, z)] = 3
+                else:
+                    blocks[flatten_coord(x, y, z)] = randint(4, 7)
+
+    return chunkX, chunkZ, blocks
     
 class World:
     def __init__(self, app):
@@ -46,10 +63,13 @@ class World:
 
                             def call(x):
                                 self.gen_queue.remove((x[0], x[1]))
+                                print(x[2])
+
+                            def error(x):
                                 print(x)
 
                             print("Adding to async")
-                            res = self.pool.apply_async(func, (x, z), callback=call)
+                            res = self.pool.apply_async(chunk_provider, (x, z, CHUNK_SIZE, CHUNK_HEIGHT, utils.flatten_coord), callback=call, error_callback=error)
 
                             self.chunks[(x, z)] = self.chunks[(0, 0)]
 
